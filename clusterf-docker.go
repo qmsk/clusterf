08719d06812c669bb173928b44f3893d8ab4356d
Tero Marttila <tero.marttila@aalto.fi>
2015-10-13 14:41:39 +0300

    clusterf-docker: configContainer()

--- clusterf-docker.go
+++ clusterf-docker.go
@@ -3,6 +3,7 @@ import (
     "qmsk.net/clusterf/config"
     "qmsk.net/clusterf/docker"
     "flag"
+    "fmt"
     "log"
     "os"
 )
@@ -27,56 +28,89 @@ type self struct {
     docker *docker.Docker
 
     // registered state
-    containerState map[string]*docker.Container
+    containerConfig map[string]*config.ConfigServiceBackend
+}
+
+// Translate a docker container to a service config
+func (self *self) configContainer (container *docker.Container) *config.ConfigServiceBackend {
+    configBackend := config.ConfigServiceBackend{}
+
+    if serviceLabel, set := container.Labels["net.qmsk.clusterf.service"]; !set {
+        return nil
+    } else {
+        configBackend.ServiceName = serviceLabel
+    }
+
+    configBackend.BackendName = container.ID
+    configBackend.Backend.IPv4 = container.IPv4.String()
+
+    for _, port := range container.Ports {
+        // limit by label?
+        portLabel := container.Labels[fmt.Sprintf("net.qmsk.clusterf.backend.%s", port.Proto)]
+
+        if portLabel != "" && portLabel != fmt.Sprintf("%v", port.Port) {
+            continue
+        }
+
+        switch port.Proto {
+        case "tcp":
+            configBackend.Backend.TCP = port.Port
+        case "udp":
+            configBackend.Backend.UDP = port.Port
+        }
+    }
+
+    return &configBackend
 }
 
 // Synchronize active container state to config
 func (self *self) syncContainer(container *docker.Container) {
-    if self.containerState[container.ID] == container {
+    containerConfig := self.configContainer(container)
+
+    if self.containerConfig[container.ID] == containerConfig {
         // no-op
         log.Printf("syncContainer %s: no-op\n", container.ID)
 
         return
     }
 
-    log.Printf("syncContainer %s: update\n", container.ID)
+    log.Printf("syncContainer %s: update: %#v\n", container.ID, containerConfig)
 
-    self.containerState[container.ID] = container
+    self.containerConfig[container.ID] = containerConfig
 }
 
-// Teardown inactive container state
-func (self *self) teardownContainer(container *docker.Container) {
-    log.Printf("teardownContainer %s\n", container.ID)
+// Teardown container state if active
+func (self *self) teardownContainer(containerID string) {
+    if containerConfig, exists := self.containerConfig[containerID]; !exists {
+        log.Printf("teardownContainer %s: unknown\n", containerID)
 
-    delete(self.containerState, container.ID)
+    } else {
+        log.Printf("teardownContainer %s: %#v\n", containerID, containerConfig)
+
+        delete(self.containerConfig, containerID)
+    }
 }
 
 // Update container state
 func (self *self) containerEvent(containerEvent docker.ContainerEvent) {
-    container := self.containerState[containerEvent.ID]
-
-    if containerEvent.State != nil {
-        container = containerEvent.State
-    }
-
-    if container == nil {
-        log.Printf("containerEvent %s:%s: unknown\n", containerEvent.Status, containerEvent.ID)
-
-    } else if !containerEvent.Running {
+    if !containerEvent.Running {
         log.Printf("containerEvent %s:%s: teardown\n", containerEvent.Status, containerEvent.ID)
 
-        self.teardownContainer(container)
+        self.teardownContainer(containerEvent.ID)
 
-    } else {
+    } else if containerEvent.State != nil {
         log.Printf("containerEvent %s:%s: sync\n", containerEvent.Status, containerEvent.ID)
 
-        self.syncContainer(container)
+        self.syncContainer(containerEvent.State)
+
+    } else {
+        log.Printf("containerEvent %s:%s: unknown\n", containerEvent.Status, containerEvent.ID)
     }
 }
 
 func main() {
     self := self{
-        containerState:  make(map[string]*docker.Container),
+        containerConfig:    make(map[string]*config.ConfigServiceBackend),
     }
 
     flag.Parse()
