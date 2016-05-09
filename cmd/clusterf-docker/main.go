8945e476fa64ca028166497cfd00cee15a2169c0
Tero Marttila <tero.marttila@aalto.fi>
2016-05-09 15:16:41 +0300

    clusterf-docker: optionally flush on exit signal

--- cmd/clusterf-docker/main.go
+++ cmd/clusterf-docker/main.go
@@ -5,54 +5,99 @@ import (
     "github.com/qmsk/clusterf/docker"
 	"github.com/jessevdk/go-flags"
     "log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 var Options struct {
 	ConfigWriter	config.WriterOptions	`group:"Config Writer"`
 	Docker			docker.Options
+
+	ExitFlush		bool	`long:"exit-flush" help:"Flush backends on exit signal" default:"true"`
 }
 
 var flagsParser = flags.NewParser(&Options,  flags.Default)
 
+// Flush service backends when stopping
+func stop(configWriter *config.Writer) {
+	log.Printf("Flush.....")
+
+	if err := configWriter.Flush(); err != nil {
+		log.Fatalf("config:Writer.Flush: %v", err)
+	} else {
+		log.Printf("Flushed")
+	}
+}
+
+// Listen for updated docker.State, compile to config.Config and update config.Writer.
+//
+// Stops on os.Signal
+func run(configWriter *config.Writer, dockerListen chan docker.State, stopChan chan os.Signal) {
+	defer stop(configWriter)
+
+	for {
+		select {
+		case dockerState, ok := <-dockerListen:
+			if !ok {
+				// docker quit? exit and restart
+				return
+			}
+
+			if config, err := makeConfig(dockerState); err != nil {
+				log.Fatalf("configContainers: %v", err)
+			} else if err := configWriter.Write(config); err != nil {
+				log.Fatalf("config:Writer.Write: %v", err)
+			} else {
+				log.Printf("Update config...")
+			}
+
+		case s := <-stopChan:
+			log.Printf("Stopping on %v...", s)
+
+			// reset signal in case stopping gets stuck
+			signal.Stop(stopChan)
+
+			return
+		}
+	}
+
+	log.Printf("Stop...")
+}
+
 func main() {
 	if _, err := flagsParser.Parse(); err != nil {
-		log.Fatalf("flags.Parser.Parse: %v\n", err)
+		log.Fatalf("flags.Parser.Parse: %v", err)
 	}
 
 	configWriter, err := Options.ConfigWriter.Writer()
 	if err != nil {
-		log.Fatalf("config.Writer: %v\n", err)
+		log.Fatalf("config.Writer: %v", err)
 	}
 
     docker, err := Options.Docker.Open()
 	if err != nil {
-        log.Fatalf("docker:Docker.Open: %v\n", err)
+        log.Fatalf("docker:Docker.Open: %v", err)
     } else {
-        log.Printf("docker:Docker.Open: %v\n", docker)
+        log.Printf("docker:Docker.Open: %v", docker)
     }
 
-    if dockerChan, err := docker.Listen(); err != nil {
-        log.Fatalf("docker:Docker.Listen: %v\n", err)
+    dockerChan, err := docker.Listen()
+	if err != nil {
+        log.Fatalf("docker:Docker.Listen: %v", err)
     } else {
-        log.Printf("docker:Docker.Listen...\n")
-
-        for dockerState := range dockerChan {
-			if config, err := makeConfig(dockerState); err != nil {
-				log.Fatalf("configContainers: %v\n", err)
-			} else if err := configWriter.Write(config); err != nil {
-				log.Fatalf("config:Writer.Write: %v\n", err)
-			} else {
-				log.Printf("Update config\n")
-			}
-        }
-    }
-
-	// cleanup
-	log.Printf("Stop..")
-
-	if err := configWriter.Close(); err != nil {
-		log.Fatalf("config:Writer.Close: %v\n", err)
+        log.Printf("docker:Docker.Listen...")
 	}
 
-	log.Printf("Exit")
+	// optionally arrange to stop on signal
+	var stopChan chan os.Signal
+
+	if Options.ExitFlush {
+		stopChan = make(chan os.Signal)
+
+		signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
+	}
+
+	// mainloop
+	run(configWriter, dockerChan, stopChan)
 }
